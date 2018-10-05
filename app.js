5c3847d730bce53bf82022c84f0344c7268f5589
santhosh <singumahanthisanthosh@gmail.com>
2018-10-05 13:09:54 +0530

    socket.io implementation

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 var app = require('express')();
 var http = require('http').Server(app);
-app.set('view engine', 'html');
+var ejs = require('ejs');
+var io = require('socket.io')(http);
+io.on('connection', function(socket){
+   //Whenever someone connects this gets executed
+    console.log('A user connected');
+  
+    //Whenever someone disconnects this piece of code executed
+    socket.on('disconnect', function () {
+      console.log('A user disconnected');
+    });
+  
+  });
+  
+app.set('view engine', 'ejs');
 app.get('/', function(req, res){
-  res.sendfile('index');
+  res.render('index');
 });
 
 http.listen(3000, function(){
